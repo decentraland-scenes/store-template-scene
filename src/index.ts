ee22d65eef58558ece82bdd7276794d74e23d220
nearnshaw <earnshaw.nico@gmail.com>
2023-07-05 11:12:00 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { engine, InputAction, inputSystem, Material, MeshCollider, pointerEventsSystem } from '@dcl/sdk/ecs'
+import { Color4 } from '@dcl/sdk/math'
+import { setupUi } from './ui'
+
+
+
+
+export function main() {
+	//setupUi()
+
+}
