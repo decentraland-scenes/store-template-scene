3db84caf22eaa9592d402b8f57526bd4e74574bc
Nicolas Earnshaw <earnshaw.nico@gmail.com>
2026-01-30 19:11:24 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { engine, InputAction, inputSystem, Material, MeshCollider, pointerEventsSystem } from '@dcl/sdk/ecs'
-import { Color4 } from '@dcl/sdk/math'
+import {} from '@dcl/sdk/math'
+import { engine } from '@dcl/sdk/ecs'
 import { setupUi } from './ui'
 
 
-
-
 export function main() {
-	//setupUi()
+    // uncomment the line below to initialize UI from ui.tsx
+    //setupUi()
 
+    // your scene code here
 }
